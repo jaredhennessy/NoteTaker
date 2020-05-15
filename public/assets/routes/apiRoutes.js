43173f248b984caa72d0e0a1e2956853f8185acb
jaredhennessy <jared.hennessy@gmail.com>
2020-05-15 15:41:24 -0700

    routes reconfigured

--- public/assets/routes/apiRoutes.js
+++ public/assets/routes/apiRoutes.js
@@ -1,7 +1,7 @@
 var noteData = require("../../data/db");
 
 module.exports = function (app) {
-  app.get("/api/tables", function (req, res) {
+  app.get("/api/notes", function (req, res) {
     res.json(noteData);
   });
 };
