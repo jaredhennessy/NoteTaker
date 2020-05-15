fc0185cbaa67b1ef30cfd3677c008ed5ffcb5a1a
jaredhennessy <jared.hennessy@gmail.com>
2020-05-15 15:43:12 -0700

    routes reconfigured

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./assets/routes/apiRoutes")(app);
+require("./assets/routes/htmlRoutes")(app);
 
 app.listen(PORT, function () {
   console.log("App listening on PORT: " + PORT);
